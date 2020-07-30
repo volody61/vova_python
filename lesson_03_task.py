5a8c680ee08db0dc482ea520ebe1f4664aa6c03b
vovaksenov <vovaksenov@gmail.com>
2020-07-30 09:59:54 +0700

    Привет мир

--- lesson_03_task.py
+++ lesson_03_task.py
@@ -312,34 +312,41 @@
 # с первой клетки на вторую одним ходом.
 # 4 4 5 5 -> YES
 
-x1 = int(input())
-y1 = int(input())
-x2 = int(input())
-y2 = int(input())
-if x1 == x2 and y1 == y2 - 1:
-    print('yes')
-elif x1 == x2 and y1 == y2 + 1:
-    print('yes')
-elif x1 == x2 - 1 and y1 == y2:
-    print('yes')
-elif x1 == x2 + 1 and y1 == y2:
-    print(yes)
-elif x1 == x2 - 1 and y1 == y2 - 1:
-    print('yes')
-elif x1 == x2 + 1 and y1 == y2 + 1:
-    print('yes')
-elif x1 == x2 - 1 and y1 == y2 + 1:
-    print('yes')
-elif x1 == x2 + 1 and y1 == y2 - 1:
-    print(yes)
-else:
-    print('no')
+# x1 = int(input())
+# y1 = int(input())
+# x2 = int(input())
+# y2 = int(input())
+# if x1 == x2 and y1 == y2 - 1:
+#     print('yes')
+# elif x1 == x2 and y1 == y2 + 1:
+#     print('yes')
+# elif x1 == x2 - 1 and y1 == y2:
+#     print('yes')
+# elif x1 == x2 + 1 and y1 == y2:
+#     print(yes)
+# elif x1 == x2 - 1 and y1 == y2 - 1:
+#     print('yes')
+# elif x1 == x2 + 1 and y1 == y2 + 1:
+#     print('yes')
+# elif x1 == x2 - 1 and y1 == y2 + 1:
+#     print('yes')
+# elif x1 == x2 + 1 and y1 == y2 - 1:
+#     print(yes)
+# else:
+#     print('no')
 
 # Шахматный слон ходит по диагонали. Даны две различные клетки шахматной доски, определите,
 # может ли слон попасть с первой клетки на вторую одним ходом.
 # 4 4 5 5 -> YES
 
-
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if abs(x1 - x2) == abs(y1 - y2):
+    print('YES')
+else:
+    print('NO')
 
 # Шахматный ферзь ходит по диагонали, горизонтали или вертикали.
 # Даны две различные клетки шахматной доски, определите, может ли ферзь
