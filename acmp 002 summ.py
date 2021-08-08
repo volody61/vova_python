bb4badaa301872c6ce576aeefb1a512d9eb65632
vovaksenov <vovaksenov@gmail.com>
2021-08-08 13:10:41 +0700

    Rename acmp files

--- acmp 002 summ.py
+++ acmp 002 summ.py
@@ -1,11 +1,5 @@
 a = int(input())
 b = 0
-if a >= 0:
-    while a >= 1:
-        b += a
-        a -= 1
-else:
-    while a <= 1:
-        b += a
-        a += 1
+for i in range(1, a + 1):
+    b += i
 print(b)
